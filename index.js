54e90c2ebec5eb53626eabf0841a5154c442507f
seyhing <bensey873@gmail.com>
2022-08-28 18:55:12 +0700

    Add hero section + responsive

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const audioBtn = document.querySelector(".audio-btn");
 const audioIcon = document.querySelector(".icon");
 const audio = document.querySelector(".audio");
-let isPaused = false;
+const playPause = document.querySelector('.play-pause');
+let isPaused = true;
 audioBtn.addEventListener("click", function() {
     // click listener for audio button
-    if (!isPaused) {
-        audioIcon.src = "./resources/img/icons8-play-30.png";
-        isPaused = true;
-        audio.pause();
-    } else {
+    if (isPaused) {
         audioIcon.src = "./resources/img/icons8-pause-30.png";
         isPaused = false;
         audio.play();
+        playPause.innerHTML = "pause";
+    } else {
+        audioIcon.src = "./resources/img/icons8-play-30.png";
+        isPaused = true;
+        audio.pause();
+        playPause.innerHTML = "play";
     }
 });
